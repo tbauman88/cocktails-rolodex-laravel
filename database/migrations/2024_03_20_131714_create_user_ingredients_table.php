1e864ebf38f8288517bb5ea99c0f98a5de8f3496
Tyler Bauman <t.bauman@vehikl.com>
2024-04-05 09:30:39 -0400

    update use ingredients tables

--- database/migrations/2024_03_20_131714_create_user_ingredients_table.php
+++ database/migrations/2024_03_20_131714_create_user_ingredients_table.php
@@ -4,18 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('user_ingredients', function (Blueprint $table) {
             $table->id();
+
+            $table->string('brand')->nullable();
+            $table->tinyText('category')->nullable();
+            $table->tinyText('sub_category')->nullable();
 
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
 
             $table->unsignedBigInteger('ingredient_id');
             $table->foreign('ingredient_id')->references('id')->on('ingredients');
+
+            $table->unique(['ingredient_id', 'brand']);
 
             $table->timestamps();
         });
