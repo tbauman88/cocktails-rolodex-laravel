70b0b3020b27d55cc3423ca98a8493a3cc92367c
Tyler Bauman <t.bauman@vehikl.com>
2024-04-08 17:17:14 -0400

    add tests for drink and ingredient models

---
+++ tests/Models/DrinkTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Drink;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+uses(TestCase::class);
+uses(DatabaseTransactions::class);
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+});
+
+
+test('it returns the ingredients for a drink', function () {
+    $drink = Drink::factory()->for($this->user)->create(['name' => 'Black Manhattan']);
+
+    $drink->ingredients()->attach([
+        ['ingredient_id' => 1, 'amount' => 1, 'amount_unit' => 'oz'],
+        ['ingredient_id' => 2, 'amount' => 1, 'amount_unit' => 'oz'],
+    ]);
+
+    $actual = $drink->ingredients();
+
+    expect($actual->get())->toHaveCount(2);
+});
+
+test('it returns the users favourited drinks', function () {
+    $drink = Drink::factory()->for($this->user)->create(['name' => 'Black Manhattan']);
+    $this->user->toggleDrink($drink);
+
+    $actual = $this->user->favouriteDrinks();
+
+    $actualName = $actual->pluck('name')->first();
+
+    expect($actual->get())
+        ->toHaveCount(1)
+        ->and($actualName)->toEqual($drink->name);
+});
---
+++ tests/Models/IngredientTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Drink;
+use App\Models\Ingredient;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+uses(TestCase::class);
+uses(DatabaseTransactions::class);
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+});
+
+
+test('it returns the drinks for an ingredient', function () {
+    $ingredient = Ingredient::factory()->create(['name' => 'Gin (Tanqueray)']);
+    $drinks = Drink::factory()->for($this->user)->count(3)->create();
+
+    [$firstDrink, $secondDrink, $thirdDrink] = $drinks;
+
+    $ingredient->drinks()->attach([
+        ['drink_id' => $firstDrink->getKey(), 'amount' => 1, 'amount_unit' => 'oz'],
+        ['drink_id' => $secondDrink->getKey(), 'amount' => 1, 'amount_unit' => 'oz'],
+    ]);
+
+    $actual = $ingredient->drinks();
+    $drinkNames = $actual->pluck('name');
+
+    expect($actual->get())->toHaveCount(2);
+    expect($drinkNames)->not->toContain($thirdDrink->name);
+});
+
